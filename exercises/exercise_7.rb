d1a9718c02385f29fe09b6b5448852f9b72a80dc
Jinny Jang <jangjinny21@gmail.com>
2021-03-02 07:12:22 +0000

    display error message

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,13 +9,12 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Add validations to two models to enforce the following business rules
-# Employees must always have a first name present
-# Employees must always have a last name present
-# Employees have a hourly_rate that is a number (integer) between 40 and 200
-# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
-# Stores must always have a name that is a minimum of 3 characters
-# Stores have an annual_revenue that is a number (integer) that must be 0 or more
-# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+print "Please enter a store name. "
+@store = gets.chomp.to_i
 
-validates :
+@new_store = Store.create( name: @store ) 
+
+@new_store.errors.each do |attribute, message|
+  puts "#{attribute}: #{message}"
+end
+
