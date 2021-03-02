3e9eed9abac191b917bf6d0cc765d5a4aafd800f
Jinny Jang <jangjinny21@gmail.com>
2021-03-02 04:52:42 +0000

    create more stores and filter by apparel

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,39 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+burnaby = Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true)
+
+whislter = Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false)
+
+yaletown = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true)
+
+# use where method to fetch stores that carry men's apparel
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where(womens_apparel: true, annual_revenue  < 1000000)
+
+def filter_mens_store(mens_stores)
+  mens_stores.each do |store|
+    puts "#{store.name} store has an annual revenue of #{store.annual_revenue}."
+  end
+end
+
+def filter_womens_store(womens_stores)
+  womens_stores.each do |store|
+    puts "#{store.name} store has an annual revenue of #{store.annual_revenue}."
+  end
+end
+
+filter_mens_store(@mens_stores)
+filter_womens_store(@womens_stores)
